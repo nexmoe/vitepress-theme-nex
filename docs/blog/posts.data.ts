05b7faea728d5dc573b00b16342461872e3312b3
Nexmoe <16796652+nexmoe@users.noreply.github.com>
2024-07-26 22:28:02 +0800

    refactor(docs): update import path and type definition for posts data
    Change the import source of NTransform and NPost types to the correct
    path in the posts.data.ts file. Also, update the type of the data
    variable to align with the new type definition.
    
    Old path: '../../src/composables/transform'
    New path: '../../src/'
    
    BREAKING CHANGE: The import paths have been updated, and the type of the
    data variable has been changed. Ensure that all references to these
    types and the data variable are updated accordingly.

--- docs/blog/posts.data.ts
+++ docs/blog/posts.data.ts
@@ -1,7 +1,7 @@
 import { createContentLoader } from 'vitepress'
-import { NTransform, type Post } from '../../src/composables/transform'
+import { NTransform, type NPost } from '../../src/'
 
-declare const data: Post[]
+declare const data: NPost[]
 export { data }
 
 export default createContentLoader('blog/posts/*.md', {
